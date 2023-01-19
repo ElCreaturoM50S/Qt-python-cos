83ba7c0ca819e9b4abaabd4ed3eb30ad85b2f7a3
ElCreaturoM50S <>
2023-01-19 12:34:15 +0100

    add radio and messagebox

--- main.py
+++ main.py
@@ -7,12 +7,31 @@ class Main(QDialog):
         self.setWindowTitle("amonger")
         
         self.text = QLabel(self)
+        self.text.setStyleSheet("color: red; font-size:15px")
         self.text.setText('bruh bruh')
-    def change_text(self):
-        self.text.setText('Tekst 2')
+        self.text.move(50,30)
+
+        self.btgColor = QButtonGroup()
+        self.rbtRed = QRadioButton("Red", self)
+        self.rbtRed.move(50,50)
+        self.rbtRed.setChecked(True)
+        self.rbtRed.clicked.connect(self.evt_rbt_clicked)
+        self.btgColor.addButton(self.rbtRed)
+
+        self.rbtGreen = QRadioButton("Green", self)
+        self.rbtGreen.move(50,80)
+        self.rbtGreen.setChecked(True)
+
+        self.rbtGreen.clicked.connect(self.evt_rbt_clicked)
+        self.btgColor.addButton(self.rbtGreen)
+    def evt_rbt_clicked(self):
+        msgBox = QMessageBox()
+        msgBox.setText("witam witam witam")
+        msgBox.exec()
+
 
 if __name__ == '__main__':
-        app = QApplication(sys)
+        app = QApplication(sys.argv)
         main = Main()
         main.show()
 
